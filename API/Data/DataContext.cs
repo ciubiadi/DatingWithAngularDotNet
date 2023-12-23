48874fd0068baf5cb252f98c0cda600b825a19d7
aciubucciu <ciubucciu.adrianalbert@gmail.com>
2023-12-23 16:57:35 +0200

    end of backend skeleton

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Data;
+
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options) {
+
+    }
+
+    // name of the table
+    public DbSet<AppUser> Users { get; set; }
+}
